84daff8e1607501e08cdb3af152d900f101c8a9d
mrbossosity <mrbossosity@gmail.com>
2020-10-07 08:08:39 -1000

    whoops not gitub

--- scripts/loader.js
+++ scripts/loader.js
@@ -1,5 +1,5 @@
 if(!window.location.hash) {
     window.location = window.location + '#loaded';
 } else {
-    window.location.href = 'https://mrbossosity.gitub.io/connect'
+    window.location.href = 'https://mrbossosity.github.io/connect'
 }
